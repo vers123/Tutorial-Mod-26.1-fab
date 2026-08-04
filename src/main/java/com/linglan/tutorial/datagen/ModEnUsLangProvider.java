46c998075dd5ed77fb0923e972cec113791e6b01
linglan <3047493305@qq.com>
2026-08-05 04:49:01 +0800

    #17-tooltip

--- src/main/java/com/linglan/tutorial/datagen/ModEnUsLangProvider.java
+++ src/main/java/com/linglan/tutorial/datagen/ModEnUsLangProvider.java
@@ -56,5 +56,8 @@ public class ModEnUsLangProvider extends FabricLanguageProvider {
 
         translationBuilder.add("itemGroup.tutorial", "Tutorial Tab");
         translationBuilder.add("itemGroup.tutorial2", "Tutorial Tab2");
+
+        translationBuilder.add("tooltip.tutorial.pickaxe_axe_tooltip.shift", "This is a item that can be used as a pickaxe and an axe");
+        translationBuilder.add("tooltip.tutorial.pickaxe_axe_tooltip", "Hold §6§n§l§oSHIFT§r§r§r§r for more info!");
     }
 }
--- src/main/java/com/linglan/tutorial/item/custom/PickaxeItem.java
+++ src/main/java/com/linglan/tutorial/item/custom/PickaxeItem.java
@@ -1,10 +1,16 @@
 package com.linglan.tutorial.item.custom;
 
 import com.linglan.tutorial.tag.ModBlockTags;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.function.Consumer;
 
 public class PickaxeItem extends AxeItem {
     public PickaxeItem(ToolMaterial material, float attackDamageBaseline, float attackSpeedBaseline, Properties properties) {
@@ -19,5 +25,14 @@ public class PickaxeItem extends AxeItem {
     @Override
     public boolean isCorrectToolForDrops(ItemStack itemStack, BlockState state) {
         return state.is(ModBlockTags.PICKAXE_AXE_MINEABLE);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, TooltipContext context, TooltipDisplay display, Consumer<Component> builder, TooltipFlag tooltipFlag) {
+        if (Minecraft.getInstance().hasShiftDown()) {
+            builder.accept(Component.translatable("tooltip.tutorial.pickaxe_axe_tooltip.shift"));
+        } else {
+            builder.accept(Component.translatable("tooltip.tutorial.pickaxe_axe_tooltip"));
+        }
     }
 }
